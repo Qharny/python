814c63a572d824a087e3c100aba5b6ec4455aeff
Qharny <Qharny>
2023-09-05 22:45:11 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    .vs/ProjectSettings.json
    	deleted:    .vs/PythonSettings.json
    	deleted:    .vs/VSWorkspaceState.json
    	deleted:    .vs/python/FileContentIndex/ba9f43d3-8330-4eb9-b766-c8aea2abca6a.vsidx
    	deleted:    .vs/python/FileContentIndex/cdb2c54f-2d65-40d7-98bb-e4a0f38f6aa7.vsidx
    	deleted:    .vs/python/v17/.wsuo
    	deleted:    .vs/slnx.sqlite
    	new file:   art.py
    	deleted:    main.dart
    	deleted:    message.dart
    	deleted:    pubspec.yaml
    	deleted:    quiz.dart

---
+++ art.py
@@ -0,0 +1,16 @@
+import turtle as T
+
+T.pensize(2)
+T.bgcolor('lavender')
+T.forward(100)  # Move the turtle forward by 100 units
+T.right(90)     # Turn the turtle right by 90 degrees
+T.forward(100)
+T.right(90)
+T.forward(100)
+T.right(90)
+T.forward(100)
+T.fillcolor('blue')
+
+
+# Close the T graphics window when done
+T.done()

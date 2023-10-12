8d18aa1839aa7cd42b76419fc75ce75fbd562c90
Qharny <cannycc1020@gmail.com>
2023-10-12 15:10:38 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .vscode/settings.json
    	new file:   myPy.py

---
+++ myPy.py
@@ -0,0 +1,51 @@
+# A python code to print your name in style
+
+# get user name
+name = input("Enter your name: ")
+
+for x in name:
+    x = x.upper()
+    if (x == "A"):
+        print(" .. ###### .. \n .. # .... w .. \n. .######. .\n .. # .. .. # .. \n .. # .... # .. \n\n")
+    elif (x == "B"):
+        print(" .. weesee .. \n .. # .... # .. \n .. gewee ... \n .. # .... # .. \n .. queeee .. \n\n")
+    elif (x == "C"):
+        print(" .. eosae# .. \n .. # ....")
+    elif (x == "D"):
+        print(" .. ##wee ... \n .. # .... # .. \n .. # .... @ .. \n ..  .... @ .. \n .. eeme ... \n\n")
+    elif (x == "E"):
+        print(" .. ###### .. \n .. # ....... \n .. ##### ... \n .. # ....... \n .. #####g .. \n\n")
+    elif (x == "F"):
+        print(" .. @##0e .. \n .. # ....... \n .. w#w#w ... \n .. # ....... \n .. # ....... \n\n")
+# elif (x == "G"):
+# print(" .. e#e#es .. \n .. # ....... \n .. seees ... \n .. # .... # .. \n .. reeee ... \n\n")
+# elff (x == "H"):
+# print(" .. # .... # .. \n .. # .... #. .\n .. weweww .. \n .. # .... # .. \n .. # .... # .. \n\n")
+# elif (x == "I ):
+# print(" .. weweew .. \n .... ew .... \n .... ee .... \n .... @w .... \n .. eweveg .. \n\n")
+# elif (x == "3"):
+# print(" .. ##e#w# .. \n .... ## .... \n .... ## .... \n .. #.e# .... \n .. #e## .... \n\n")
+# elif (x == "K"):
+# print(". .e. . . #. . . \n. .#. . @. . . . \n. . #. . . . . .\n .. \n. .. .. .\n\n")
+# elif (x == "L ):
+# print(" .. # ....
+# elif (x == "M"):
+# print(" .. # .... # .. \n .. #w .. @# .. \n. .#. w#.s. .\n. .# .... #. .\n .. # .... # .. \n\n")
+# elif (x == "N"):
+# print(" ....... u .. \n .. #e ... #. .\n .. #. #. . g. .\n .... #.#. .\n .. # ... ## .. \n\n")
+# elif (x == "0"):
+# print(" .. #eeses .. \n .. # .... # .. \n. .e .... #. .\n .. # .. . .#. . \n. .ese# .. \n\n")
+# elif (x =m "P"):
+# print(" .. wawgee .. \n .. # .... # .. \n .. wweeww. .\n ...... ... \n .. w ....... \n\n")
+# elif (x == "Q"):
+# print(" .. ####e# .. \n .. # .... # .. \n .. #.# .. # .. \n .. # .. #.# .. \n ..= eew#e .. \n\n")
+# elif (x =- "R"):
+# print(" .. ##w#ww .. \n .. # .... # .. \n .. #.##. .. \n .. # ... #. .. \n. .#. ... # .. \n\n")
+# elif (x == "5 ):
+# print(" .. seeeee .. \n .. # ..
+# elif (x == "T"):
+# print(" .. #####e .. \n .... #e .... \n .... ## .... \n .... #e .... \n .... ## .... \n\n")
+# elif (x =m "U"):
+# print(" ....... # .. \n .. # ..
+# elif (x == "V"):
+# \n ... # .. g ... \n .... ## .... \n\n")
